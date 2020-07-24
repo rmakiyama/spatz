57cd9a3f09496a0861a48cd2dbef57ba9ce08feb
rmakiyama <rmakiyama.io@gmail.com>
2020-07-24 23:41:01 +0900

    insetter setup

--- app/src/main/java/com/rmakiyama/spatz/ui/MainActivity.kt
+++ app/src/main/java/com/rmakiyama/spatz/ui/MainActivity.kt
@@ -1,8 +1,16 @@
 package com.rmakiyama.spatz.ui
 
+import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.rmakiyama.spatz.R
 import dagger.hilt.android.AndroidEntryPoint
+import dev.chrisbanes.insetter.Insetter
 
 @AndroidEntryPoint
-class MainActivity : AppCompatActivity(R.layout.activity_main)
+class MainActivity : AppCompatActivity(R.layout.activity_main) {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        Insetter.setEdgeToEdgeSystemUiFlags(window.decorView, true)
+    }
+}
