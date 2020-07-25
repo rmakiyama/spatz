a8de9cd2f5401da5e20a32fb93ab496ec9534670
rmakiyama <rmakiyama.io@gmail.com>
2020-07-25 13:41:02 +0900

    add login ui

--- features/auth/src/main/java/com/rmakiyama/spatz/auth/LoginFragment.kt
+++ features/auth/src/main/java/com/rmakiyama/spatz/auth/LoginFragment.kt
@@ -1,5 +1,7 @@
 package com.rmakiyama.spatz.auth
 
+import android.os.Bundle
+import androidx.activity.OnBackPressedCallback
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import dagger.hilt.android.AndroidEntryPoint
@@ -8,4 +10,15 @@ import dagger.hilt.android.AndroidEntryPoint
 class LoginFragment : Fragment(R.layout.fragment_login) {
 
     private val viewModel: LoginViewModel by viewModels()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        requireActivity().onBackPressedDispatcher.addCallback(this, closeLoginOnBackPressed)
+    }
+
+    private val closeLoginOnBackPressed = object : OnBackPressedCallback(true) {
+        override fun handleOnBackPressed() {
+            activity?.finish()
+        }
+    }
 }
